068eadd504c8b1490f0aeddfd1a75334643da7d8
MichaelMitch <mitchellwyne@gmail.com>
2023-04-17 21:46:48 -0700

    start web page build

--- surfsup/app.py
+++ surfsup/app.py
@@ -1,30 +1,44 @@
 # Import the dependencies.
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+from flask import Flask, jsonify
 
 
 
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///hawaii.sqlite",)
 
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
 
 # Save references to each table
-
+Hawaii = Base.measurements.stations
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = FLask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def main():
+    return (
+        
